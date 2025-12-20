b4c26bbf193a9f89cd7d72927f48e7ba585040fd
João Victor <joaovictorsalotialves12@gmail.com>
2025-12-20 13:52:52 -0300

    Filtrando pelo status

--- src/database/database.js
+++ src/database/database.js
@@ -31,8 +31,17 @@ export class Database {
     return data
   }
 
-  select(table) {
+  select(table, filters) {
     let data = this.#database[table] ?? []
+
+    if (filters) {
+      data = data.filter((row) => {
+        return Object.entries(filters).some(([key, value]) => {
+          return row[key].toLowerCase().includes(value.toLowerCase())
+        })
+      })
+    }
+
     return data
   }
 }
