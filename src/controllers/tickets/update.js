15b7479e85246202973f3a619224f355ab72fc80
João Victor <joaovictorsalotialves12@gmail.com>
2025-12-20 14:09:58 -0300

    Obtendo dados do ticket

--- src/controllers/tickets/update.js
+++ src/controllers/tickets/update.js
@@ -1,3 +1,6 @@
-export function update({request, response, database}) {
-  return response.end('Update ticket endpoint')
+export function update({ request, response, database }) {
+  const { id } = request.params
+  const { equipment, description  } = request.body
+
+  return response.writeHead(204).end()
 }
