17bcd5db62147858ce51a94b8bc0059bc3b7a244
João Victor <joaovictorsalotialves12@gmail.com>
2025-12-20 14:33:38 -0300

    Atualizando o status do ticket

--- src/controllers/tickets/updateStatus.js
+++ src/controllers/tickets/updateStatus.js
@@ -1,3 +1,10 @@
 export function updateStatus({ request, response, database }) {
-  return response.end('ok')
+  const { id } = request.params
+
+  database.update('tickets', id, {
+    status: 'closed',
+    updated_at: new Date(),
+  })
+
+  return response.writeHead(204).end()
 }
